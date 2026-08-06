fe43eb83662a05e95497b18f7ad4957c06c51f9e
tsmoxefreefire-maker <tsmoxefreefire@gmail.com>
2026-08-06 15:44:48 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,28 @@
 import os
-import fastapi
+from fastapi import FastAPI
 from openai import OpenAI
 
-app = fastapi.FastAPI()
+app = FastAPI()
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 @app.get("/User_name&Age")
 def get_user_name_and_age(name: str, age: int):
     return {"your Name is:": name, "your Age is:": age}
+
+
 @app.get("/ask_ai")
 def ask_ai(user_question: str):
+    messages_list: list = [
+        {"role": "system", "content": "you are a helpful coding tutor"},
+        {"role": "user", "content": user_question},
+    ]
+
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages =[
-            {"role": "system", "content": "you are a helpful coding tutor"},
-            {"role": "user", "content": user_question},
-        ],
+        model="gpt-3.5-turbo",
+        messages=messages_list,
     )
 
     answer = response.choices[0].message.content
-
     return {"question": user_question, "ai_answer": answer}
