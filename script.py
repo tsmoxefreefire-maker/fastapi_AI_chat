25285b3f5ad7103631a36c04920f3f1adc13c6fb
tsmoxefreefire-maker <tsmoxefreefire@gmail.com>
2026-08-06 15:52:53 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import os
 from fastapi import FastAPI
-from openai import OpenAI
+from google import genai
 
 app = FastAPI()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 @app.get("/User_name&Age")
@@ -14,15 +14,9 @@ def get_user_name_and_age(name: str, age: int):
 
 @app.get("/ask_ai")
 def ask_ai(user_question: str):
-    messages_list: list = [
-        {"role": "system", "content": "you are a helpful coding tutor"},
-        {"role": "user", "content": user_question},
-    ]
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages_list,
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_question,
     )
 
-    answer = response.choices[0].message.content
-    return {"question": user_question, "ai_answer": answer}
+    return {"question": user_question, "ai_answer": response.text}
