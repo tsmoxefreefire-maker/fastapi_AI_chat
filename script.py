e3221aff2f3b9474647ddb3c79428475f92ccef4
tsmoxefreefire-maker <tsmoxefreefire@gmail.com>
2026-08-06 15:19:50 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,24 @@
+import os
+import fastapi
+from openai import OpenAI
+
+app = fastapi.FastAPI()
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "sk-YOUR_ACTUAL_KEY"))
+
+@app.get("/User_name&Age")
+def get_user_name_and_age(name: str, age: int):
+    return {"your Name is:": name, "your Age is:": age}
+@app.get("/ask_ai")
+def ask_ai(user_question: str):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages =[
+            {"role": "system", "content": "you are a helpful coding tutor"},
+            {"role": "user", "content": user_question},
+        ],
+    )
+
+    answer = response.choices[0].message.content
+
+    return {"question": user_question, "ai_answer": answer}
